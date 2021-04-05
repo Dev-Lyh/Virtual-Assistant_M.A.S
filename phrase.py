491d3f6248abadd44b5f2762bf64efbc0a55eebd
EmilyFelicio <66891508+EmilyFelicio@users.noreply.github.com>
2021-04-05 14:37:12 -0300

    falas.py
    The phrases in be here!!!

---
+++ phrase.py
@@ -0,0 +1,15 @@
+cumprimentar = ['Oi', 'Olá', 'Oi, tudo bem?', 'Olá, como vai?']
+
+como_esta = ['Estou bem, e você?', 'Comigo está tudo ótimo, e como vai você?']
+
+humano_bem = ['Que bom, como posso ajudá-lo?', 'Ótimo, gostaria de ajuda?']
+
+dnd = ['De nada', 'Estarei aqui se precisar']
+
+piadas = ['O que o tomate foi fazer no banco? Tirar extrato. KKKKK', 'Qual é o doce preferido do átomo? o Pé-de-Moléculas. RRa RRa']
+
+outra_piada = ['Qual é o prato que ninguém consegue fazer direito? A torta.', 'Se o papai Noel morrer ele não estara mais "em-trenós" HAHA']
+
+mais_piada = ['O que é um pontinho amarelo na limusine? "É um milhonário"','Qual é o som que os porcos fazem quando se beijam? "Ui"']
+
+dont_understand = ['Poderia repetir, eu não entendi', 'Eu não consegui entender o que você falou', 'Estou em dúvida, poderia repetir?']
